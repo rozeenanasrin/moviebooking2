ca2dcd538ed602f9c649068d9b470eced50d8814
rozeenanasrin <53082598+rozeenanasrin@users.noreply.github.com>
2023-10-29 23:36:24 +0530

    Create db.config.js

---
+++ config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://rozeenanasrin:rozeenanasrin@cluster0.p5lxxzv.mongodb.net/?retryWrites=true&w=majority', // Replace with your MongoDB Atlas connection string
+};
